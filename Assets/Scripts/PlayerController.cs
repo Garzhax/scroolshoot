160f8994023e093efab32cf44c802ac29f1af671
Garzhax <alandiazcabj12@gmail.com>
2025-06-17 18:29:22 -0300

    movimiento con el vector 2d

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,29 +3,19 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public GameObject bulletPrefab;
-    public Transform firePoint;
-
     private Vector2 moveInput;
+    public float moveSpeed = 5f;
+
+    // Este método se llama automáticamente con Send Messages
+    public void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
 
     void Update()
     {
-        Vector3 move = new Vector3(moveInput.x, moveInput.y, 0);
+        // Movimiento en plano X-Z
+        Vector3 move = new Vector3(moveInput.x, 0, moveInput.y);
         transform.Translate(move * moveSpeed * Time.deltaTime);
     }
-
-    public void OnMove(InputAction.CallbackContext context)
-    {
-        moveInput = context.ReadValue<Vector2>();
-    }
-
-    public void OnFire(InputAction.CallbackContext context)
-    {
-        if (context.performed)
-        {
-            Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-        }
-    }
 }
-
